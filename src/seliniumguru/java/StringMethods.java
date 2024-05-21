32a15ce850eb66088e371476fd317b43eafa5c44
charrai <clt86@zips.uakron.edu>
2024-05-21 15:23:03 -0400

    My first git

---
+++ src/seliniumguru/java/StringMethods.java
@@ -0,0 +1,20 @@
+package seliniumguru.java;
+public class StringMethods {
+    public static void main(String args[]) {
+        String str1 = "Hello students";
+        String str2 = "Welcome";
+        String str3 = "TEST";
+        String str4 = "Java";
+        String str5 = "     string    ";
+        int totalStudents = 50;
+        String str6 = "abc def ghi";
+        String strCount= str6.split(//s+");
+                System.out.println(str1.concat(str2));
+        System.out.println(str1.replace("e", "a"));
+        System.out.println(str3.toLowerCase());
+        System.out.println(str4.toUpperCase());
+        System.out.println(str5.trim());
+
+    }
+}
+
